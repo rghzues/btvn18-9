322ffa1d101994cf55b2d94f9dd57e8df50f686b
Rogere <hadungtan130706@gmail.com>
2025-09-20 10:27:42 +0700

    first commit

---
+++ baitat.js
@@ -0,0 +1,21 @@
+
+let a = 2;
+let b = 3;
+
+
+let tong = a + b;
+let hieu = a - b;
+let tich = a * b;
+let thuong = a / b;
+let phandu = a % b;
+
+console.log("Bài 1: Tính toán với a = " + a + ", b = " + b);
+console.log("Tổng: " + tong);
+console.log("Hiệu: " + hieu);
+console.log("Tích: " + tich);
+console.log("Thương: " + thuong);
+console.log("Phần dư: " + phandu);
+
+
+let luythua = Math.pow(a, b);
+console.log("Bài 2: " + a + "^" + b + " = " + luythua);
